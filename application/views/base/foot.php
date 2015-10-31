e6154546a4cc21029e6fffec518a4c9d91c49e7e
ko <ko@localhost.localdomain>
2015-10-31 10:09:20 -0430

    eliminando archivos css y js duplicados en sistema sigpa

--- application/views/base/foot.php
+++ application/views/base/foot.php
@@ -14,15 +14,15 @@
 </div>
 <i id="loading" class="fa fa-refresh fa-spin fa-3x fa-fw infoZone" title="Cargando.."></i>
 <!-- Bibliotecas plantilla -->
-<script src="<?= base_url('assets/css/template/bower_components/jquery/dist/jquery.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/metisMenu/dist/metisMenu.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/dist/js/sb-admin-2.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/datatables/media/js/jquery.dataTables.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/datepicker/js/bootstrap-datepicker.min.js') ?>"></script>
-<script src="<?= base_url('assets/css/template/bower_components/datepicker/locales/bootstrap-datepicker.es.min.js') ?>"></script>
-<script src="<?= base_url('assets/lib/sigpa.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/metisMenu/dist/metisMenu.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/dist/js/sb-admin-2.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/datatables/media/js/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/datepicker/js/bootstrap-datepicker.min.js') ?>"></script>
+<script src="<?= base_url('../css/template/bower_components/datepicker/locales/bootstrap-datepicker.es.min.js') ?>"></script>
+<script src="<?= base_url('../lib/sigpa.js') ?>"></script>
 
 <script>
     $(document).ready(loading(false));
