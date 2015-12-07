f2c69e7a2e977bc5002024e539d7a7d0b37dff3c
kuai-mare orion <orionkmc@gmail.com>
2015-12-07 00:25:36 -0430

    modificando desplazamiento de la modal

--- application/views/schedules/index.php
+++ application/views/schedules/index.php
@@ -25,7 +25,7 @@
         $("#cerrar-modal").on("click", function(){
             if($("#cerrar-modal").attr('value') == 'cerrar')
             {
-                $("#modal").css('left', '-40%');
+                $("#modal").css('left', '-20%');
                 $("#box_schedule").css('width', '100%');
                 $("#box_schedule").css('transition', 'all 1s');
                 $("#label-mostrar-modal").css('transition', 'all 1s');
